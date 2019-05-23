25d9d7cfe104c34af71a6cf24d8bad40bd54f0a4
Rodrigo Jacinto <rodrigojacinto16@gmail.com>
2019-05-23 14:24:36 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ let express = require('express')
 
 let app = express();
 
-consign({cwd: process.cwd()+"/app"}).include('config').then('app/models').then('app/routes').into(app);
+consign().include('config').then('app/models').then('app/routes').into(app);
