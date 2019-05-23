afb7e8b83f829641a747db58eae0f016cd552c3d
Rodrigo Jacinto <rodrigojacinto16@gmail.com>
2019-05-23 12:45:38 -0300

    Update login.js

--- app/routes/login.js
+++ app/routes/login.js
@@ -4,7 +4,7 @@ module.exports = app => {
 
         let loginDao = app.app.models.loginDao;
         loginDao.valida(res, req.body);
-
+        console.log('Models aqui: ',app.app.models);
     });
 
 
