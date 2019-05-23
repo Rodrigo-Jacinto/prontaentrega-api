aedecc6ed9ad2c70f18f6dea46d612d7b8d38bd1
Rodrigo Jacinto da Silva <rodrigojacinto16@gmail.com>
2019-05-23 01:18:12 -0300

    Ajustando cors da api 2

--- config/server.js
+++ config/server.js
@@ -7,7 +7,7 @@ module.exports = app => {
     app.use(bodyParser.json());
 
     app.use(function (req, res, next) {
-        res.set("Access-Control-Allow-Origin", "https://prontaentrega.herokuapp.com/");
+        res.set("Access-Control-Allow-Origin", "https://prontaentrega.herokuapp.com");
         res.set("Vary", "Origin");
         res.header("Access-Control-Allow-Headers", "Content-Type");
         res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
